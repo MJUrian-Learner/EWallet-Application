12e334bb246f705848601031999622544dbce74d
MJ Urian <michael.urian@gmail.com>
2024-04-09 21:10:23 +0800

    Added More UI Components on Main Menu Activity

--- app/src/main/java/com/example/ewalletapplication/MainMenu.java
+++ app/src/main/java/com/example/ewalletapplication/MainMenu.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
 public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        ImageSlider imageSlider = findViewById(R.id.imageSlider);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.img_slide_one, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.img_slide_two, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.img_slide_three, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.img_slide_four, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.img_slide_five, ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT);
     }
 }
